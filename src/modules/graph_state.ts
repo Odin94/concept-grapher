368805760188548d0e6b6c6fccc6b55654dcd0ef
Odin <kammerloher.andreas@gmail.com>
2018-09-08 15:36:41 +0200

    Add temporary node feature to graph_state

--- src/modules/graph_state.ts
+++ src/modules/graph_state.ts
@@ -1,19 +1,42 @@
+import { Text } from 'pixi.js';
+import constants from '../constants';
+
 export class GraphState {
-    constructor(public nodes: Array<GraphNode>) { };
+    private temporary_node: GraphNode = null;
+
+    constructor(public nodes: Array<GraphNode> = [], public connections: Array<NodeConnection> = []) { };
 
     add(node: GraphNode) {
         this.nodes.push(node);
     };
 
-    remove(nodeId: string) {
+    remove(nodeId: number) {
         this.nodes = this.nodes.filter(elem => elem.id !== nodeId);
+    };
+
+    create_temporary_node(x: number, y: number) {
+        this.store_and_null_temporary_node();
+
+        const new_id = this.get_max_id() + 1;
+        const new_text = new Text("", constants.DEFAULT_FONT);
+        this.temporary_node = new GraphNode(new_id, new_text);
+    }
+
+    private store_and_null_temporary_node() {
+        this.add(this.temporary_node);
+        this.temporary_node = null;
+    }
+
+    private get_max_id(): number {
+        const ids: Array<number> = this.nodes.map((node) => node.id);
+        return ids.length > 0 ? Math.max(...ids) : 0;
     }
 }
 
 export class GraphNode {
-    constructor(public readonly id: string, public text: string, public color: string, public connections: Array<NodeConnection>) { };
+    constructor(public readonly id: number, public text: Text) { };
 }
 
 export class NodeConnection {
-    constructor(public readonly firstNodeId: string, public readonly secondNodeId: string, public text: string) { };
+    constructor(public readonly firstNodeId: number, public readonly secondNodeId: string, public text: Text) { };
 }
