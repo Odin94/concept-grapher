2111d22a9a33d5a3bb38d17a7d4105edb193d62c
Odin <kammerloher.andreas@gmail.com>
2018-09-09 14:21:10 +0200

    Use new graph storage path & graph-state json functions

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export default {
     DEFAULT_FONT: { fontFamily: 'Arial', fontSize: 32, fill: 'black' },
+    GRAPH_STORAGE_PATH: "stored_graphs"
 }
--- src/modules/state_persister.ts
+++ src/modules/state_persister.ts
@@ -1,13 +1,15 @@
-import { GraphState } from "./graph_state";
+import { GraphState, JSONableGraphState } from "./graph_state";
 import { writeFile, readFileSync } from 'fs';
+import { join } from 'path';
+import constants from "../constants";
 
 export default class StatePersister {
     constructor() { };
 
     store_graph(graph: GraphState, filename: string) {
-        const json_graph = graph.to_json();
+        const json_graph = JSON.stringify(graph.to_jsonanble_graph_state(), null, 2);
 
-        writeFile(filename, json_graph, err => {
+        writeFile(join(constants.GRAPH_STORAGE_PATH, filename), json_graph, err => {
             if (err) {
                 console.log(err);
                 // TODO: display that there was an error somewhere
@@ -19,9 +21,17 @@ export default class StatePersister {
     }
 
     load_graph(filename: string, viewport: Viewport): GraphState | null {
-        const json_graph = readFileSync(filename);
+        const json_graph = readFileSync(join(constants.GRAPH_STORAGE_PATH, filename));
         if (json_graph) {
-            return new GraphState(viewport);
+            try {
+                const jsonable_graph_state: JSONableGraphState = JSON.parse(json_graph.toString());
+                return jsonable_graph_state.to_graph_state(viewport);
+            } catch (err) {
+                // TODO: display error?
+                console.log(err);
+
+                return null;
+            }
         }
 
         // TODO: display error?
