0b530040519c7127cef496149fac4dceceb18ebd
Odin <kammerloher.andreas@gmail.com>
2018-09-09 13:02:04 +0200

    Store temporary node on de-selecting

--- src/modules/mouse_input.ts
+++ src/modules/mouse_input.ts
@@ -3,7 +3,7 @@ import { Point } from 'pixi.js';
 import Controls from './controls';
 
 export default class MouseInput {
-    constructor(public viewport: Viewport, public graphState: GraphState, public controls: Controls) {
+    constructor(public viewport: Viewport, public graph_state: GraphState, public controls: Controls) {
         this.addMouseListeners();
     };
 
@@ -12,10 +12,12 @@ export default class MouseInput {
             console.log(data);
             const selected_node = this.select_node(data.world as Point);
             if (selected_node) {
+                this.graph_state.store_and_null_temporary_node();
                 this.controls.set_selected_node(selected_node);
             } else {
-                this.graphState.create_temporary_node(data.world);
-                this.controls.set_selected_node(this.graphState.temporary_node as GraphNode);
+                this.graph_state.store_and_null_temporary_node();
+                this.graph_state.create_temporary_node(data.world);
+                this.controls.set_selected_node(this.graph_state.temporary_node as GraphNode);
             }
         });
 
@@ -31,11 +33,11 @@ export default class MouseInput {
         });
     }
 
-    private select_node(mousePoint: Point): GraphNode | null {
-        const allNodes: Array<GraphNode> = this.graphState.temporary_node ? this.graphState.nodes.concat(this.graphState.temporary_node) : this.graphState.nodes;
+    private select_node(mouse_point: Point): GraphNode | null {
+        const all_nodes: Array<GraphNode> = this.graph_state.temporary_node ? this.graph_state.nodes.concat(this.graph_state.temporary_node) : this.graph_state.nodes;
 
-        return allNodes.find(({ text }) => {
-            return text.containsPoint(mousePoint);
+        return all_nodes.find(({ text }) => {
+            return text.containsPoint(mouse_point);
         }) || null;
     }
 }
