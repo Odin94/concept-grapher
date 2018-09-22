3c9a6c6501524be87015543ce4c316f681696cae
Odin <kammerloher.andreas@gmail.com>
2018-09-22 11:54:28 +0200

    Implement destroy_temporary_node()

--- src/modules/graph/graph_state.ts
+++ src/modules/graph/graph_state.ts
@@ -1,9 +1,71 @@
 import { Text, PointLike } from 'pixi.js';
 import constants from '../../constants';
-import StatePersister from '../state_persister';
 import { GraphNode, JSONableGraphNode } from './graph_node';
 import { NodeConnection, JSONableNodeConnection } from './node_connection';
-import { ConceptGrapher } from '../concept_grapher';
+
+export class GraphState {
+    temporary_node: GraphNode | null = null;
+
+    constructor(public viewport: Viewport, public nodes: Array<GraphNode> = [], public connections: Array<NodeConnection> = []) { };
+
+    add(node: GraphNode) {
+        this.nodes.push(node);
+        this.viewport.addChild(node.text);
+    };
+
+    remove(nodeId: number) {
+        this.nodes = this.nodes.filter(elem => elem.id !== nodeId);
+
+        const removed_node = this.nodes.find(elem => elem.id !== nodeId);
+        if (removed_node) this.viewport.removeChild(removed_node.text);
+    };
+
+    create_temporary_node(mouse_point: PointLike) {
+        this.store_and_null_temporary_node();
+
+        const new_id = this.get_max_id() + 1;
+        const new_text = new Text("TEST_REMOVE_THIS", constants.DEFAULT_FONT);
+        new_text.position.set(mouse_point.x, mouse_point.y);
+        this.temporary_node = new GraphNode(new_id, new_text);
+
+        this.viewport.addChild(this.temporary_node.text);
+    };
+
+    destroy_temporary_node() {
+        if (this.temporary_node === null) return;
+
+        this.viewport.removeChild(this.temporary_node.text);
+        this.temporary_node = null;
+    };
+
+    // TODO: find a better name - store implies persistance, but this just "stores" to the in-memory graph
+    store_and_null_temporary_node() {
+        if (this.temporary_node === null) return;
+
+        this.viewport.removeChild(this.temporary_node.text);
+        this.add(this.temporary_node);
+        this.temporary_node = null;
+    };
+
+    to_jsonanble_graph_state(): JSONableGraphState {
+        const jsonable_nodes: Array<JSONableGraphNode> = [];
+        for (const node of this.nodes) {
+            jsonable_nodes.push(node.to_jsonable_graph_node());
+        }
+
+        const jsonable_connections: Array<JSONableNodeConnection> = [];
+        for (const connection of this.connections) {
+            jsonable_connections.push(connection.to_jsonable_node_connection());
+        }
+
+        return new JSONableGraphState(jsonable_nodes, jsonable_connections);
+    };
+
+    private get_max_id(): number {
+        const ids: Array<number> = this.nodes.map((node) => node.id);
+        return ids.length > 0 ? Math.max(...ids) : 0;
+    };
+};
 
 export class JSONableGraphState {
     constructor(public nodes: Array<JSONableGraphNode>, public connections: Array<JSONableNodeConnection>) { }
@@ -24,67 +86,5 @@ export class JSONableGraphState {
         }
 
         return new GraphState(viewport, loaded_nodes, loaded_connections);
-    }
-};
-
-export class GraphState {
-    temporary_node: GraphNode | null = null;
-
-    constructor(public viewport: Viewport, public nodes: Array<GraphNode> = [], public connections: Array<NodeConnection> = []) { };
-
-    add(node: GraphNode) {
-        this.nodes.push(node);
-        this.viewport.addChild(node.text);
-
-        new StatePersister().store_graph(this, "filename.json");
-    };
-
-    remove(nodeId: number) {
-        this.nodes = this.nodes.filter(elem => elem.id !== nodeId);
-
-        const removed_node = this.nodes.find(elem => elem.id !== nodeId);
-        if (removed_node) this.viewport.removeChild(removed_node.text);
-    };
-
-    create_temporary_node(mouse_point: PointLike) {
-        this.store_and_null_temporary_node();
-
-        const new_id = this.get_max_id() + 1;
-        const new_text = new Text("TEST_REMOVE_THIS", constants.DEFAULT_FONT);
-        new_text.position.set(mouse_point.x, mouse_point.y);
-        this.temporary_node = new GraphNode(new_id, new_text);
-
-        this.viewport.addChild(this.temporary_node.text);
-    }
-
-    destroy_temporary_node() {
-        // TODO: remove temporary node's text from viewport
-    }
-
-    store_and_null_temporary_node() {
-        if (this.temporary_node == null) return;
-
-        this.viewport.removeChild(this.temporary_node.text);
-        this.add(this.temporary_node);
-        this.temporary_node = null;
-    }
-
-    to_jsonanble_graph_state(): JSONableGraphState {
-        const jsonable_nodes: Array<JSONableGraphNode> = [];
-        for (const node of this.nodes) {
-            jsonable_nodes.push(node.to_jsonable_graph_node());
-        }
-
-        const jsonable_connections: Array<JSONableNodeConnection> = [];
-        for (const connection of this.connections) {
-            jsonable_connections.push(connection.to_jsonable_node_connection());
-        }
-
-        return new JSONableGraphState(jsonable_nodes, jsonable_connections);
-    }
-
-    private get_max_id(): number {
-        const ids: Array<number> = this.nodes.map((node) => node.id);
-        return ids.length > 0 ? Math.max(...ids) : 0;
     }
 };
